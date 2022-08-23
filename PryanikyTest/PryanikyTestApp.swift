256cea08aa78a10a176b4e284fbb7244a6257e10
Artem Paliutin <pal.artem@icloud.com>
2022-08-23 19:27:57 +0300

    Initial Commit

---
+++ PryanikyTest/PryanikyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PryanikyTestApp.swift
+//  PryanikyTest
+//
+//  Created by Artem Paliutin on 23/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct PryanikyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
